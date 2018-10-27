ffacab9f1f079c2add6d62e4ad0c7dd9ceceac59
Daniel Tait <daniel.tait@skyscanner.net>
2018-10-27 15:02:08 +0100

    added string scanner

---
+++ src/assembler/stringScanner.js
@@ -0,0 +1,57 @@
+class StringScanner {
+    constructor(str) {
+        this.str = str
+        this.lines = str.split(/\n/);
+        this.offset = 0
+    }
+
+    getCoords() {
+        var line = 0;
+        var col = 0;
+        var currentOffset = 0;
+
+        while (currentOffset + this.lines[line].length < this.offset) {
+            currentOffset += this.lines[line].length + 1;
+            line++
+        }
+
+        return {
+            line: line + 1,
+            column: (this.offset - currentOffset) + 1
+        };
+    }
+
+    eof() {
+        return this.str.length == 0;
+    }
+
+    scan(expr) {
+        const i = this.str.search(expr);
+        if (i != 0)
+            return false;
+
+        const match = this.str.match(expr)[0];
+        const length = match.length;
+        this.str = this.str.slice(length);
+        this.offset += length;
+
+        return match;
+    }
+
+    next(expr) {
+        const i = this.str.search(expr);
+        return i == 0;
+    }
+
+    skipToNextChar() {
+        this.scan(/\s*/);
+        while (true) {
+            if (!this.scan('//'))
+                break;
+            this.scan(/.*/);
+            this.scan(/\s+/);
+        }
+    }
+}
+
+export default StringScanner;
---
+++ tests/assembler/stringScanner.test.js
@@ -0,0 +1,198 @@
+import StringScanner from 'chip8/assembler/stringScanner.js';
+
+
+describe('getCoords', () => {
+    test('returns expected coords at start', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+
+        expect(scanner.getCoords()).toEqual({
+            line: 1,
+            column: 1
+        });
+    });
+
+    test('returns expected coords first line', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan('Hello');
+
+        expect(scanner.getCoords()).toEqual({
+            line: 1,
+            column: 6
+        });
+    });
+
+    test('returns expected coords second line', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan('Hello world');
+        scanner.scan(/\s+/);
+        scanner.scan('some');
+
+        expect(scanner.getCoords()).toEqual({
+            line: 2,
+            column: 5
+        });
+    });
+
+    test('returns expected coords eos', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan(/.*/s);
+
+        expect(scanner.getCoords()).toEqual({
+            line: 2,
+            column: 10
+        });
+    });
+});
+
+describe('eof', () => {
+    test('returns true at end of string', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan(/.*/s);
+
+        expect(scanner.eof()).toBe(true);
+    });
+
+    test('returns false if not at end of string', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan('Hello');
+
+        expect(scanner.eof()).toBe(false);
+    });
+});
+
+describe('scan', () => {
+    test('returns matched string', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        const result = scanner.scan(/\w+/);
+
+        expect(result).toEqual('Hello');
+    });
+
+    test('iterates through string', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan('Hello world');
+        scanner.scan(/\s+/);
+        const result = scanner.scan(/[^:]+/);
+
+        expect(result).toEqual('some');
+    });
+
+    test('returns false for no match', () => {
+        const str = "Hello world\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        const result = scanner.scan(/\s+/);
+
+        expect(result).toBe(false);
+    });
+});
+
+describe('next', () => {
+    test('returns true if next matches', () => {
+        const str = "Hello world\n" +
+                    "// this is a comment\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan('Hello ');
+
+        const result = scanner.next('world');
+
+        expect(result).toBe(true);
+    });
+
+    test('returns false if next does not match', () => {
+        const str = "Hello world\n" +
+                    "// this is a comment\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+
+        const result = scanner.next('world');
+
+        expect(result).toBe(false);
+    });
+
+    test('does not consume string', () => {
+        const str = "Hello world\n" +
+                    "// this is a comment\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.next('Hello');
+
+        const result = scanner.scan('Hello');
+
+        expect(result).toBe('Hello');
+    });
+});
+
+describe('skipToNextChar', () => {
+    test('ignores comments on lines', () => {
+        const str = "Hello world\n" +
+                    "// this is a comment\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan('Hello world')
+        scanner.skipToNextChar();
+
+        const result = scanner.scan(/\w+/);
+
+        expect(result).toBe('some');
+    });
+
+    test('ignores inline comments', () => {
+        const str = "Hello world // this comment is inline\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.scan('Hello world')
+        scanner.skipToNextChar();
+
+        const result = scanner.scan(/\w+/);
+
+        expect(result).toBe('some');
+    });
+
+    test('ignores multiline whitespace', () => {
+        const str = "\n" +
+                    "\n" +
+                    "\n" +
+                    "// a comment\n" +
+                    "Hello world // this comment is inline\n" +
+                    "some:more";
+
+        const scanner = new StringScanner(str);
+        scanner.skipToNextChar();
+
+        const result = scanner.scan('Hello world');
+
+        expect(result).toBe('Hello world');
+    });
+});
