80f75761e593675e2908358b8292787363620bde
Daniel Tait <dantait91@gmail.com>
2019-01-03 18:09:14 +0000

    adding feature flag for material ui dev

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import Button from '@material-ui/core/Button'; // eslint-disable-line no-unused-vars
+
+const App = () => (
+    <Button variant="contained" color="primary">
+        A material UI... soon
+    </Button>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import 'normalize.css';
-import 'chip8/styles/main.scss';
-
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
-import App from 'chip8/components/app.js'; // eslint-disable-line no-unused-vars
 import Cpu from 'chip8/cpu/cpu.js';
 import Display from 'chip8/gfx/display.js';
 
@@ -12,4 +8,17 @@ const cpu = new Cpu(display);
 
 cpu.reset();
 
-ReactDOM.render(<App cpu={cpu}/>, document.getElementById('app'));
+const urlParams = new URLSearchParams(window.location.search);
+const useMaterial = (/^true$/i).test(urlParams.get('material'));
+
+if (useMaterial) {
+    import('chip8/components/App').then(({default: App}) => { // eslint-disable-line no-unused-vars
+        ReactDOM.render(<App cpu={cpu}/>, document.getElementById('app'));
+    });
+} else {
+    import('normalize.css');
+    import('chip8/styles/main.scss');
+    import('chip8/components/app.js').then(({default: App}) => { // eslint-disable-line no-unused-vars
+        ReactDOM.render(<App cpu={cpu}/>, document.getElementById('app'));
+    });
+}
