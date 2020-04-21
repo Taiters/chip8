ec63df7ce62db73891a86a3333a8661e39a190ee
Daniel Tait <daniel.tait@skyscanner.net>
2020-04-21 21:43:05 +0100

    changing files to check build issue

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,4 +1,25 @@
-import Editor from './editor.js';
+import brace from 'brace'; // eslint-disable-line no-unused-vars
+import 'brace/theme/gruvbox';
+
+import React, {
+    useState
+} from 'react';
+import AceEditor from 'react-ace';
+
+
+const Editor = () => {
+    const [value, setValue] = useState('Playing about with UI\nBout it');
+
+    return (
+        <AceEditor 
+            width='100%'
+            height='100%'
+            value={value}
+            onChange={setValue}
+            setOptions={{ printMargin: null }}
+            theme='gruvbox' />
+    );
+};
 
 
 export default Editor;
--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,4 +1,47 @@
-import Screen from './screen';
+import React, { 
+    useState,
+    useEffect,
+    useCallback
+} from 'react';
+
+
+const Screen = ({gfx}) => {
+    const [ctx, setCtx] = useState(null);
+    const captureCtx = useCallback(canvas => {
+        if (canvas !== null) {
+            setCtx(canvas.getContext('2d'));
+        }
+    });
+
+    useEffect(() => {
+        if (ctx === null)
+            return;
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.fillRect(0, 0, 640, 320);
+        ctx.fillStyle = 'rgb(0, 255, 255)';
+
+        for (let i = 0; i < 64 * 32; i++) {
+            if (gfx[i] !== 1)
+                continue;
+
+            const x = (i % 64) * 10;
+            const y = (i / 64) * 10;
+
+            ctx.fillRect(x, y, 10, 10);
+        }
+    }, [gfx, ctx]);
+
+    return (
+        <canvas 
+            style={{
+                width: '100%'
+            }}
+            ref={captureCtx} 
+            width="640" 
+            height="320" >
+        </canvas>
+    );
+};
 
 
 export default Screen;
